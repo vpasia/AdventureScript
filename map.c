bde60860b18f64f19ef663e4139d05bcc70bcf02
YungRiceBowl <vincentpaulmpasia@gmail.com>
2024-07-30 01:34:32 -0400

    Revamped Map implementation to deal with collisions and proper resizing

--- map.c
+++ map.c
@@ -1,5 +1,7 @@
 #include "map.h"
 
+#include <string.h>
+
 
 static uint64_t hash_key(const char* key)
 {
@@ -13,6 +15,17 @@ static uint64_t hash_key(const char* key)
 	}
 
 	return hash;
+}
+
+static char* strdupl(const char* s) 
+{
+	size_t len = strlen(s) + 1;
+	char* copy = malloc(len);
+	if (copy) 
+	{
+		memcpy(copy, s, len);
+	}
+	return copy;
 }
 
 
@@ -32,42 +45,127 @@ Map* createMap()
 		return NULL;
 	}
 
+	int i;
+	for(i = 0; i < m->capacity; i++)
+	{
+		m->entries[i].key = NULL;
+		m->entries[i].next = NULL;
+	}
+
 	return m;
 }
 
 void freeMap(Map* map)
 {
+	int i;
+	for(i = 0; i < map->capacity; i++)
+	{    
+		MapEntry* previousEntry = map->entries[i].next;
+		MapEntry* currentEntry = previousEntry;
+
+		while(currentEntry != NULL)
+		{
+			previousEntry = currentEntry;
+			currentEntry = currentEntry->next;
+			free((void*)previousEntry->key);
+			free(previousEntry);
+		}
+	}
+
 	free(map->entries);
 	free(map);
 }
 
-bool setItem(Map* map, const char* key, void* value)
+bool resizeMap(Map* map)
 {
-	if(map->count == map->capacity)
+	size_t newCapacity = map->capacity * 2;
+	MapEntry* newEntries = calloc(newCapacity, sizeof(MapEntry));
+
+	if(newEntries == NULL) return false;
+
+	int i;
+	for(i = 0; i < map->capacity; i++)
 	{
-		size_t newSize = sizeof(MapEntry) * (map->capacity) * 2;
+		MapEntry* entry = &map->entries[i];
 
-		MapEntry* tmpEntries = realloc(map->entries, newSize);
+		uint64_t hash = hash_key(entry->key);
+		size_t newIndex = (size_t)(hash & (uint64_t)(newCapacity - 1));
 
-		if(tmpEntries != NULL)
+		MapEntry* newEntry = &newEntries[newIndex];
+
+		while(entry != NULL)
 		{
-			map->entries = tmpEntries;
-			map->capacity *= 2;
-		}
-		else
-		{
-			return false;
+			newEntry->key = entry->key;
+			newEntry->value = entry->value;
+			newEntry->next = entry->next;
+
+			newEntry = newEntry->next;
+			entry = entry->next;
 		}
 	}
 
+	free(map->entries);
+	map->entries = newEntries;
+	map->capacity = newCapacity;
+
+	return true;
+}
+
+bool setItem(Map* map, const char* key, void* value)
+{
+	if(map->count == map->capacity && !resizeMap(map)) return false;
 
 	uint64_t hash = hash_key(key);
 	size_t index = (size_t)(hash & (uint64_t)(map->capacity - 1));
 
 	if(map->entries[index].key == NULL) map->count++;
 
-	map->entries[index].key = strdup(key);
-	map->entries[index].value = value;
+	MapEntry* currentEntry = &map->entries[index];
+	MapEntry* previousEntry = NULL;
+
+	while(currentEntry != NULL && currentEntry->key != NULL)
+	{
+		if(strcmp(currentEntry->key, key) == 0)
+		{
+			break;
+		}
+		previousEntry = currentEntry;
+		currentEntry = currentEntry->next;
+	}
+
+	if(currentEntry == NULL)
+	{
+		if(previousEntry != NULL)
+		{
+			currentEntry = malloc(sizeof(MapEntry));
+			if(currentEntry == NULL)
+			{
+				return false;
+			}
+			previousEntry->next = currentEntry;
+		}
+
+		currentEntry->key = strdupl(key);
+		if(currentEntry->key == NULL)
+		{
+			return false;
+		}
+
+		currentEntry->value = value;
+		currentEntry->next = NULL;
+	}
+	else
+	{
+		free((void*)currentEntry->key);
+		currentEntry->key = strdupl(key);
+		if(currentEntry->key == NULL)
+		{
+			return false;
+		}
+
+		currentEntry->value = value;
+	}
+
 	return true;
 }
 
@@ -76,12 +174,18 @@ void* getItem(Map* map, const char* key)
 	uint64_t hash = hash_key(key);
 	size_t index = (size_t)(hash & (uint64_t)(map->capacity - 1));
 
-	char* retrievedKey = (char*)map->entries[index].key;
+	MapEntry* currentEntry = &map->entries[index];
+	printf("%s - %s at index %zu with pointer %p\n", currentEntry->key, key, index, currentEntry);
 
-	if(retrievedKey == NULL)
+	while(currentEntry != NULL && currentEntry->key != NULL)
 	{
-		return NULL;
+		if(strcmp(currentEntry->key, key) == 0)
+		{
+			return currentEntry->value;
+		}
+
+		currentEntry = currentEntry->next;
 	}
 
-	return map->entries[index].value;
+	return NULL;
 }
